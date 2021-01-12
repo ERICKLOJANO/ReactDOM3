afb537f8b02b81cc8fdc40202ea7cb86d0f46168
Roberto C Melchor <rmelchor151@gmail.com>
2021-01-12 14:43:02 -0500

    setup table component

---
+++ react-dom-3/src/components/Table.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Tablerow from './tableRow'
+
+class Table extends React.Component {
+
+    render() {
+
+        return (
+            //testing fucntionality of tablerow, need to add rows and cells on button clicks
+            <table>
+                <Tablerow />
+            </table>
+        )
+    }
+}
+
+export default Table
